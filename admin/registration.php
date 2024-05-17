e437cb08fb62b214b3e83aa2b9d2bd6462c1c900
HUMURA ELVIN <humura53@gmail.com>
2024-05-17 07:21:09 +0200

    changes made

--- admin/registration.php
+++ admin/registration.php
@@ -89,7 +89,7 @@ type: "POST",
 url: "get_seater.php",
 data:'roomid='+val,
 success: function(data){
-//alert(data);
+
 $('#seater').val(data);
 }
 });
@@ -99,7 +99,7 @@ type: "POST",
 url: "get_seater.php",
 data:'rid='+val,
 success: function(data){
-//alert(data);
+
 $('#fpm').val(data);
 }
 });
