41d2400b91ba635f8ba56b0be7088a06f3cb3e6a
HUMURA ELVIN <humura53@gmail.com>
2024-05-23 10:16:05 +0200

    changes made

--- index.php
+++ index.php
@@ -181,7 +181,7 @@
 	 <div class="wrapper">
         <section class="form login">
           <header>Login To your Account</header>
-          <form action="#" method='POST'>
+          <form action="#" method='POST' auto-complete='off'>
             <div class="error-txt"></div>
             <div class="field input">
               <label>Email Adress / User-name</label>
