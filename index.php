8a6077c9733f6b300e70c2358f794118bd69f728
HUMURA ELVIN <humura53@gmail.com>
2024-05-23 15:33:23 +0200

    changes made

--- index.php
+++ index.php
@@ -1,43 +1,4 @@
-<?php
-session_start();
-
-$host = 'localhost';
-
-$user = 'root';
-
-$password = '';
-
-$db_name = 'hostel';
-
-$port = 3306;
-
-$conn = mysqli_connect($host, $user, $password, $db_name, $port);
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $email = $_POST['email'];
-  $password = $_POST['password'];
-
-  $sql = "SELECT * FROM users WHERE username = ? AND password = ?";
-  $stmt = $conn->prepare($sql);
-  $stmt->bind_param("ss", $email, $password);
-  $stmt->execute();
-  $result = $stmt->get_result();
-
-  $row = mysqli_fetch_array($result);
-
-  if ($row['usertype'] == "user") {
-     header("location:dashboard.php");
-  } elseif ($row['usertype'] == "admin") {
-     header("location:admin/dashboard.php");
-  } else {
-     echo "<script type='text/javascript'>
-        window.alert('Invalid credentials provided');
-     </script>";
-  }
-}
-?>
-
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en" class="no-js">
 <head>
 	<meta charset="UTF-8">
@@ -189,7 +150,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	 <div class="wrapper">
         <section class="form login">
           <header>Login To your Account</header>
-          <form action="#" method='POST' auto-complete='off'>
+          <form action="login_check.php" method='POST' auto-complete='off'>
             <div class="error-txt"></div>
             <div class="field input">
               <label>Email Adress</label>
