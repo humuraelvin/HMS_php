02a6ef56e64115b6f0d2b2413ac2bcd8e0eb845a
HUMURA ELVIN <humura53@gmail.com>
2024-05-23 09:49:57 +0200

    changes made

--- index.php
+++ index.php
@@ -1,11 +1,32 @@
 <?php
-session_start();
-include('includes/config.php');
-if(isset($_POST['login']))
-{
-$emailreg=$_POST['emailreg'];
-$password=$_POST['password'];
-}
+    session_start();
+    include_once "config.php";
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    if(!empty($email) && !empty($password)){
+        $sql = mysqli_query($conn, "SELECT * FROM users WHERE email = '{$email}'");
+        if(mysqli_num_rows($sql) > 0){
+            $row = mysqli_fetch_assoc($sql);
+            $user_pass = md5($password);
+            $enc_pass = $row['password'];
+            if($user_pass === $enc_pass){
+                $status = "Active now";
+                $sql2 = mysqli_query($conn, "UPDATE users SET status = '{$status}' WHERE unique_id = {$row['unique_id']}");
+                if($sql2){
+                    $_SESSION['unique_id'] = $row['unique_id'];
+                    echo "success";
+                }else{
+                    echo "Something went wrong. Please try again!";
+                }
+            }else{
+                echo "Email or Password is Incorrect!";
+            }
+        }else{
+            echo "$email - This email not Exist!";
+        }
+    }else{
+        echo "All input fields are required!";
+    }
 ?>
 
 <!doctype html>
@@ -160,7 +181,7 @@ $password=$_POST['password'];
 	 <div class="wrapper">
         <section class="form login">
           <header>Login To your Account</header>
-          <form action="#">
+          <form action="#" method='POST'>
             <div class="error-txt"></div>
             <div class="field input">
               <label>Email Adress / User-name</label>
